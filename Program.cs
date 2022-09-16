e71b5a55475d9ee89bd854241ec8a50fc88a8950
bli1962 <beeeeeen.li@outlook.com>
2022-09-16 13:01:21 +1000

    Initial version

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinServiceDemo
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+
+            if (!Environment.UserInteractive)
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[] { new ServiceDemo() };
+                ServiceBase.Run(ServicesToRun);
+            }
+            else
+            {
+                ServiceDemo service = new ServiceDemo();
+                service.StartService(null);
+
+                Console.ReadLine();
+                service.StopService();
+            }
+
+            //ServiceBase[] ServicesToRun;
+            //ServicesToRun = new ServiceBase[]
+            //{
+            //    new Service1()
+            //};
+            //ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ ServiceDemo.cs
@@ -0,0 +1,87 @@
+﻿using log4net;
+using Autofac;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Config;
+using System.Configuration;
+using System.Threading;
+
+namespace WinServiceDemo
+{
+    public partial class ServiceDemo : ServiceBase
+    {
+        private readonly ILog _logWriter = LogManager.GetLogger(typeof(ServiceDemo));
+        private readonly ManualResetEvent _shutdownEvent = new ManualResetEvent(false);
+
+        private readonly string _environment;
+        private readonly int _shutdownInterval = 60000;
+        private readonly int _runInterval = 1000;
+
+        public ServiceDemo()
+        {
+            InitializeComponent();
+            XmlConfigurator.Configure();
+
+            if (ConfigurationManager.AppSettings["Environment"] != null)
+            {
+                _environment = ConfigurationManager.AppSettings["Environment"];
+            }
+        }
+
+        internal void StartService(string[] args)
+        {
+            OnStart(args);
+        }
+
+        internal void StopService()
+        {
+            OnStop();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //OnStart(args);
+            _logWriter.InfoFormat("GFile Logging Service Starting...");
+
+            ParallelRun();
+
+            _logWriter.InfoFormat("GFile Logging Service Started.");
+
+        }
+
+        private void ParallelRun()
+        {
+            //throw new NotImplementedException();
+            Parallel.Invoke(
+                              () =>
+                                 {
+                                     Task taskA = Task.Factory.StartNew(() => new ServiceDemo().taskWriteLogs());
+                                     taskA.Wait(_runInterval);
+                                 }
+                             ); //end of parallel invoke
+        }
+
+        protected void taskWriteLogs()
+        {
+            do
+            {
+                _logWriter.InfoFormat("Run time environment {0}", _environment);
+            }
+            while (!_shutdownEvent.WaitOne(_shutdownInterval));
+        }
+
+        protected override void OnStop()
+        {
+            //OnStop();
+            _logWriter.InfoFormat("GFile Logging Service Stopping...");
+            _logWriter.InfoFormat("GFile Logging Service Stopped.");
+        }
+    }
+}
